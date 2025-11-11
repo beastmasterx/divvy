2d5098cf2b4acb7489913ee0bfe204fb6280fa0b
Jon X <ousiax@hotmail.com>
2025-11-11 22:56:11 +0800

    refactor: Remove _router postfix from router imports

--- app/api/routers/v1/__init__.py
+++ app/api/routers/v1/__init__.py
@@ -4,22 +4,22 @@ API v1 routers.
 
 from fastapi import APIRouter
 
-from .categories import router as categories_router
-from .members import router as members_router
-from .periods import router as periods_router
-from .settlement import router as settlement_router
-from .system import router as system_router
-from .transactions import router as transactions_router
+from .categories import router as categories
+from .members import router as members
+from .periods import router as periods
+from .settlement import router as settlement
+from .system import router as system
+from .transactions import router as transactions
 
 # Create API v1 router
 api_router = APIRouter(prefix="/v1")
 
 # Include all v1 routers
-api_router.include_router(members_router)
-api_router.include_router(transactions_router)
-api_router.include_router(periods_router)
-api_router.include_router(settlement_router)
-api_router.include_router(system_router)
-api_router.include_router(categories_router)
+api_router.include_router(members)
+api_router.include_router(transactions)
+api_router.include_router(periods)
+api_router.include_router(settlement)
+api_router.include_router(system)
+api_router.include_router(categories)
 
 __all__ = ["api_router"]
