cc5a1456b9bf5ea31424ac7ab76d36183f429b74
Jon X <ousiax@hotmail.com>
2025-11-26 04:31:19 +0800

    fix: Update HTTP_422_UNPROCESSABLE_ENTITY to HTTP_422_UNPROCESSABLE_CONTENT

--- app/exceptions/__init__.py
+++ app/exceptions/__init__.py
@@ -42,7 +42,7 @@ class BusinessRuleError(HTTPException):
     """Raised when a business rule is violated."""
 
     def __init__(self, detail: str):
-        super().__init__(status_code=status.HTTP_422_UNPROCESSABLE_ENTITY, detail=detail)
+        super().__init__(status_code=status.HTTP_422_UNPROCESSABLE_CONTENT, detail=detail)
 
 
 class InternalServerError(HTTPException):
